d4d5976404b51d24e962c47f4f8b2a01da65f879
kesleymartins <kesley.martins@vivaldi.net>
2024-10-08 00:34:27 -0300

    add Views dos controladores base

---
+++ src/Views/contacts/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda</title>
+</head>
+<body>
+    <h1>Contacts#index</h1>
+</body>
+</html>
+
