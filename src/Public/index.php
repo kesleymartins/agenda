ba49199ed48ecbfcdac1defed1998aa6674f919b
kesleymartins <kesley.martins@vivaldi.net>
2024-10-09 20:15:05 -0300

    update root route

--- src/Public/index.php
+++ src/Public/index.php
@@ -3,14 +3,13 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Agenda\Controllers\ContactsController;
-use App\Agenda\Controllers\HomeController;
 use App\Agenda\Controllers\PeopleController;
 use App\Agenda\Core\Router;
 
 $router = new Router();
 
-$router->add('/', HomeController::class, 'index');
 $router->add('/contacts', ContactsController::class, 'index');
 $router->add('/users', PeopleController::class, 'index');
+$router->add('/', PeopleController::class, 'index');
 
 $router->dispatch($_SERVER['REQUEST_URI']);
