53d59ae8133ae7f1d9fbff1ad10473eeb4462df5
kesleymartins <kesley.martins@vivaldi.net>
2024-10-09 21:45:37 -0300

    add PeopleRepository

---
+++ src/Repositories/PersonRepository.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Agenda\Repositories;
+
+use App\Agenda\Entities\Person;
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * @extends EntityRepository<Person>
+ */
+class PersonRepository extends EntityRepository
+{
+    //
+}
