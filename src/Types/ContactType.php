7b17bb16bf80274b829e94b2cfe87a128342f401
kesleymartins <kesley.martins@vivaldi.net>
2024-10-10 18:55:59 -0300

    add label function to display translated item name

--- src/Types/ContactType.php
+++ src/Types/ContactType.php
@@ -6,4 +6,12 @@ enum ContactType: int
 {
     case PhoneNumber = 1;
     case Email = 2;
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Email => 'E-mail',
+            self::PhoneNumber => 'Telefone'
+        };
+    }
 }
