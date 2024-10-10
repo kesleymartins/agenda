34dcd8d93597bf11afbf30fa58d4fe9e5916f66b
kesleymartins <kesley.martins@vivaldi.net>
2024-10-09 21:46:32 -0300

    pass entityManager to controller

--- src/Core/Router.php
+++ src/Core/Router.php
@@ -2,14 +2,17 @@
 
 namespace App\Agenda\Core;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class Router
 {
     private array $routes = [];
 
+    public function __construct(private EntityManagerInterface $entityManager) {}
+
     public function add(string $route, string $controller, string $method): void
     {
         $route = preg_replace('/:[a-z_]+/', '\d+', $route);
-
         $this->routes[] = [
             'route' => $route,
             'controller' => $controller,
@@ -23,7 +26,7 @@ class Router
             $pattern = '@^' . $route['route'] . '$@';
 
             if (preg_match($pattern, $requestedRoute)) {
-                $controller = new $route['controller'];
+                $controller = new $route['controller']($this->entityManager);
 
                 preg_match_all('/\d+/', $requestedRoute, $params);
 
