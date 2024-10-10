8781cabe96b0da63ac8c53409f256a05e73cb49a
kesleymartins <kesley.martins@vivaldi.net>
2024-10-09 21:45:57 -0300

    instance PersonRepository

--- src/Controllers/PeopleController.php
+++ src/Controllers/PeopleController.php
@@ -2,8 +2,21 @@
 
 namespace App\Agenda\Controllers;
 
+use App\Agenda\Entities\Person;
+use App\Agenda\Repositories\PersonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
 class PeopleController extends AbstractController
 {
+    private PersonRepository $personRepository;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+
+        $this->personRepository = $entityManager->getRepository(Person::class);
+    }
+
     public function index(): void
     {
         echo $this->twig->render('index.twig');
