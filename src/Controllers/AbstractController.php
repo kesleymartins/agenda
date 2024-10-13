68076e44d6e7de74d2c103a0cf1cc5212fcf75f7
kesleymartins <kesley.martins@vivaldi.net>
2024-10-13 20:54:49 -0300

    Update AbstractController

--- src/Controllers/AbstractController.php
+++ src/Controllers/AbstractController.php
@@ -2,27 +2,17 @@
 
 namespace App\Agenda\Controllers;
 
-use Twig\Environment;
+use App\Agenda\Core\Response;
+use Symfony\Component\Validator\Validation;
 
 class AbstractController
 {
-    protected Environment $twig;
+    protected Response $response;
+    protected $validator;
 
     public function __construct()
     {
-        $this->twigSetup();
-    }
-
-    private function twigSetup(): void
-    {
-        $class_path = explode('\\', get_class($this));
-        $class = end($class_path);
-        $controller = str_replace('Controller', '', $class);
-
-        $loader = new \Twig\Loader\FilesystemLoader();
-        $loader->addPath(__DIR__ . '/../Views/templates');
-        $loader->addPath(__DIR__ . '/../Views/' . strtolower($controller) . '/');
-
-        $this->twig = new \Twig\Environment($loader);
+        $this->response = new Response(get_class($this));
+        $this->validator = Validation::createValidatorBuilder()->enableAttributeMapping()->getValidator();
     }
 }
